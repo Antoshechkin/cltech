59ad5a3c1d2567bf51e7529189690673f345f1b4
AAntoshechkin <AAntoshechkin@srn.beeline.ru>
2025-03-19 20:14:34 +0300

    initial commit

---
+++ src/main/java/com/example/cltech/CltechApplication.java
@@ -0,0 +1,13 @@
+package com.example.cltech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CltechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CltechApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cltech/CltechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cltech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CltechApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
